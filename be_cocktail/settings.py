c5f7aefc783e7166f3299dd8b97bf7870ce74378
nimeshakya <nimeshakyabtw@gmail.com>
2024-03-19 23:00:13 +0545

    given logout permission to anyone

--- be_cocktail/settings.py
+++ be_cocktail/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 # This is to allow cors to interact with react
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
-    'http://localhost:5173'
+    'http://127.0.0.1:5173'
 ]
 
 CORS_ALLOW_CREDENTIALS = True
